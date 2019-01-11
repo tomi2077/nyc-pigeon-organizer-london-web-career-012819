de123e1946f591012f824282713ee9cccae4e768
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-11 21:09:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attr, infos|
+    infos.each do |identity, names|
+      names.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        
+        if !new_hash.has_key?(attr)
+          new_hash[name][attr] = []
+        end
+        
+        if !new_hash[name][attr].include?(identity)
+          new_hash[name][attr] << identity.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
+
